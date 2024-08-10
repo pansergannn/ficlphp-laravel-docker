2da9fce7f4311b97e3152ece9477fa0b98f6bdd1
sergannn <su.vasiliev@gmail.com>
2024-08-10 23:58:04 +0300

    a
    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,10 +40,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceRootUrl(Config::get('app.url'));
+       /* URL::forceRootUrl(Config::get('app.url'));
     if (str_contains(Config::get('app.url'), 'https://')) {
         URL::forceScheme('https');
-    }
+    }*/
         Paginator::useBootstrap();
     }
 }
